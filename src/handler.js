6426f28d73d9b31b9e068b8834035fddd68f264a
marsel945 <rewoherson519@gmail.com>
2023-12-28 12:22:35 +0800

    feat add book api

---
+++ src/handler.js
@@ -0,0 +1,69 @@
+const {nanoid} = require('nanoid')
+const books = require('./books')
+
+const addBookHandler = (request, h) => {
+  const {name, year, author, summary, publisher, pageCount, readPage} = request.payload;
+
+  const id = nanoid(16)
+  const finished = Boolean(readPage === pageCount)
+  const reading = Boolean(false)
+  const insertedAt = new Date().toISOString()
+  const updatedAt = insertedAt
+
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    insertedAt,
+    updatedAt
+  }
+
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku'
+    })
+
+    response.code(400)
+    return response
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+    })
+
+    response.code(400)
+    return response
+  }
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id
+      }
+    })
+
+    response.code(201)
+    return response
+  }
+}
+
+
+module.exports = {
+  addBookHandler,
+};
